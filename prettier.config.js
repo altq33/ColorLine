de4a55b7074ec1b4d2b10bc05f4ccd49b27127d8
ChankovDS <ChankovDS@abdgtl.com>
2026-02-23 18:19:47 +0400

    Переход на tanstack start

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+//  @ts-check
+
+/** @type {import('prettier').Config} */
+const config = {
+  semi: true,
+  singleQuote: false,
+  jsxSingleQuote: true,
+  tabWidth: 2, 
+  useTabs: true,
+  arrowParens: "always",
+  trailingComma: "all",
+};
+
+export default config;
